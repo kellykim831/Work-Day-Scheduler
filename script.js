da58f02fcfc4c04b9e0ad1d4ab2933133202a4c9
Kelly Kim <kellykim408@gmail.com>
2020-09-25 15:55:07 -0700

    add HH to the format for hourly time

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
    
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
-    var curTime = moment().format('hh');
+    var curTime = moment().format('HH');
     console.log(curTime);
 
     var times = ['9AM', '10AM', '11AM', '12PM', '1PM','2PM','3PM','4PM','5PM']
