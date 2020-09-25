099e7333703341d30fe17d5571943326119c4f14
Kelly Kim <kellykim408@gmail.com>
2020-09-25 15:42:24 -0700

    create times array and added classes

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 
+   
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    var curTime = moment().format('hh');
+    console.log(curTime);
 
+    var times = ['9AM', '10AM', '11AM', '12PM', '1PM','2PM','3PM','4PM','5PM']
+   
 
-    
+})
 
-});
