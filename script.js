0fd0adeea17333248159775531388b4c492d9b26
Kelly Kim <kellykim408@gmail.com>
2020-09-26 00:36:08 -0700

    create button container

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 
 $(document).ready(function () {
-
-   
+    var x = localStorage.getItem('notes');
+    document.getElementsByClassName('schedule').innerHTML = x;
+      
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
-    var curTime = parseInt(moment().format('HH'));
+    // var curTime = parseInt(moment().format('HH'));
+    var curTime = 10;
 
     var times = ['9AM', '10AM', '11AM', '12PM', '1PM','2PM','3PM','4PM','5PM'];
    
     for (let i = 0; i < times.length; i++) {
         $(".container").append("<div class='input-group input-group-lg hour-container'></div>");
         $(".container").append("<div class='input-group-text hour' id='inputGroup-sizing-lg'>" + times[i] + "</div>");
-        $(".container").append("<div class='form-control schedule'>Schedule</div>");
-        // $(".hour-container").append("<div class='button-container input-group-append'></div>");
-        // $(".button-container").append("<button type='button' class='btn btn-info'>Save</button>");
+        $(".container").append("<input type='text' class='form-control schedule' aria-label='Sizing example input' aria-describedby='inputGroup-sizing-lg'>");
+        $(".container").append("<div class='button-container input-group-append'></div>");
+
+        // var createBtn = document.createElement("button");
+        // createBtn.setAttribute("type", "button");
+        // createBtn.setAttribute("class", "btn btn-info");
+        // createBtn.innerHTML = "Save";
+
+        // buttonContainer.appendChild(createBtn);
+        $(".button-container").add("<button type='button' class='btn btn-info'>Save</button>");
         
         var timeNum = parseInt(times[i].slice(0, times[i].length - 2));
         if(timeNum >= 1 && timeNum <= 5){
@@ -20,8 +29,9 @@ $(document).ready(function () {
         }
         console.log(timeNum);
         console.log(curTime);
-
+        
         if(timeNum < curTime){
+            console.log('GREY');
             //set css style
             $(".schedule").css("background-color", "grey");
         } else if(timeNum > curTime){
@@ -32,5 +42,23 @@ $(document).ready(function () {
             $(".schedule").css("background-color", "red");
         }
     }
+
+    $('.schedule').text(localStorage.getItem('notes'));
+    $('button').click(function(){
+        localStorage.setItem('notes',$('input').val());
+        $('.schedule').text($('input').val());
+    })
+
+    // if(localStorage.getItem('schedule') === ''){
+    //     $('input').addClass('future');
+    // }
+
+
+
+
+
+
+
 })
+
 
